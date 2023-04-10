11f131e16e1fa9a75d0133eafbc699f54aa6393f
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-04-10 08:01:57 +0800

    update: proxy

--- server/server.py
+++ server/server.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/updateWeather')
-def updateWeather():
-    return {'weather': ['test1', ['test2'], ['test3']]}
+@app.route('/')
+@app.route('/index')
+def index():
+    return {'testKey': [1, 2, 3]}
 
 if __name__ == '__main__':
     app.run(debug=True)
